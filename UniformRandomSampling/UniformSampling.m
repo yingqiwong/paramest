dd72a9230260d1e00c9683e2cf873f2a6d8208c4
Ying-Qi Wong <yqwong25@gmail.com>
2020-12-14 17:32:25 +0000

    removing parfor from UniformSampling - causing more problems with solver timeout

--- UniformRandomSampling/UniformSampling.m
+++ UniformRandomSampling/UniformSampling.m
@@ -20,24 +20,16 @@ function [m, dhat, flag, RunTime] = UniformSampling (...
 % YQW, 13 Dec 2020
 %
 
-% set parallel for loop options
-if (usOpts.Parallel)
-    NumWorkers = usOpts.Ncores; 
-else
-    NumWorkers = 0;
-end
-
 % initialize output matrices
 Nvar    = size(mbnds,1);
 dhat    = cell(usOpts.Ntot, Ndata);
 flag    = nan(usOpts.Ntot,1);
 RunTime = flag;
 
-
 % uniform random sampling for model params
 m  = mbnds(:,1)' + diff(mbnds,[],2)'.*rand(usOpts.Ntot, Nvar);
 
-% counter for parfor blocks
+% counter for evaluation blocks before saving
 Nloop = 0;
 
 % initialize file
@@ -47,16 +39,14 @@ if usOpts.save
 end
 
 % start running models. 
-% Note nested loops so we can save data after Nit iterations in parfor loop. 
+% Note nested loops so we can save data after Nit iterations in outer loop. 
 while Nloop < usOpts.Ntot
     
-    if (usOpts.Parallel), parpool(NumWorkers); end
     
-    parfor (ins = Nloop+(1:usOpts.Nit), NumWorkers)
-        df = dhatFunc;
+    for ins = Nloop+(1:usOpts.Nit)
         
         tic;
-        [dhatIter, flag(ins)] = df(m(ins,:));
+        [dhatIter, flag(ins)] = dhatFunc(m(ins,:));
         RunTime(ins) = toc;
         
         if flag(ins)==1, dhat(ins,:) = dhatIter; end
@@ -67,16 +57,11 @@ while Nloop < usOpts.Ntot
     save(usOpts.filename, ...
         'Nloop', 'mNames', 'mbnds', 'usOpts', 'm', 'dhat', 'flag', 'RunTime');
     
-    delete(gcp('nocreate'));
 
     % move to next block of runs
     Nloop = Nloop + usOpts.Nit;
     
 end
-
-
-delete(gcp('nocreate'));
-
 
 
 end
